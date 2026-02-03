42d792451d20a5bd76b7bf716fb15bf5b7fa27b4
avi212003 <chauhanavi212003@gmail.com>
2026-02-02 19:32:07 -0500

    updated

--- models/User.js
+++ models/User.js
@@ -1,12 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
 
 const userSchema = new mongoose.Schema({
-  clerkId: {
-    type: String,
-    required: [true, 'Clerk ID is required'],
-    unique: true,
-    index: true,
-  },
   name: {
     type: String,
     required: [true, 'Name is required'],
@@ -19,6 +15,12 @@ const userSchema = new mongoose.Schema({
     lowercase: true,
     trim: true,
     match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email'],
+  },
+  password: {
+    type: String,
+    required: [true, 'Password is required'],
+    minlength: [6, 'Password must be at least 6 characters'],
+    select: false, // Don't return password by default
   },
   role: {
     type: String,
@@ -35,6 +37,8 @@ const userSchema = new mongoose.Schema({
     trim: true,
     unique: true,
   },
+  resetPasswordToken: String,
+  resetPasswordExpire: Date,
   createdAt: {
     type: Date,
     default: Date.now,
@@ -45,10 +49,48 @@ const userSchema = new mongoose.Schema({
   },
 });
 
-// Update updatedAt before saving
-userSchema.pre('save', function (next) {
-  this.updatedAt = Date.now();
-  next();
+// Hash password before saving
+userSchema.pre('save', async function (next) {
+  // Only hash if password is modified
+  if (!this.isModified('password')) {
+    this.updatedAt = Date.now();
+    return next();
+  }
+
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    this.updatedAt = Date.now();
+    next();
+  } catch (error) {
+    next(error);
+  }
 });
+
+// Method to compare password
+userSchema.methods.comparePassword = async function (candidatePassword) {
+  try {
+    return await bcrypt.compare(candidatePassword, this.password);
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+// Method to generate password reset token
+userSchema.methods.getResetPasswordToken = function () {
+  // Generate token
+  const resetToken = crypto.randomBytes(20).toString('hex');
+
+  // Hash token and set to resetPasswordToken field
+  this.resetPasswordToken = crypto
+    .createHash('sha256')
+    .update(resetToken)
+    .digest('hex');
+
+  // Set expire time (10 minutes)
+  this.resetPasswordExpire = Date.now() + 10 * 60 * 1000;
+
+  return resetToken;
+};
 
 module.exports = mongoose.model('User', userSchema);
